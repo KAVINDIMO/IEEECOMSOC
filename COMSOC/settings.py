fbaf976fc928be573d6d9c00cc7a2d4bd7675105
KAVINDIMO <kavindimo8@gmail.com>
2022-04-14 00:31:50 +0530

    K

--- COMSOC/settings.py
+++ COMSOC/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -164,9 +164,9 @@ if DEBUG:
         STATICFILES_DIRS = [
             os.path.join(BASE_DIR, 'static')
        ]
-else:
-        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,'static')
 # ]
